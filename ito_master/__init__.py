20f1b12d9e8c1da920572c96ee6c06986ae9559a
Tony <junghyun.koo@sony.com>
2025-06-20 15:56:19 +0900

    update requirements

--- ito_master/__init__.py
+++ ito_master/__init__.py
@@ -2,7 +2,7 @@ from .networks import Dasp_Mastering_Style_Transfer, Effects_Encoder, TCNModel
 from .modules import AudioFeatureLoss, CLAPFeatureLoss, Audio_Effects_Normalizer, lufs_normalize
 
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 # This defines what gets imported when someone does "from ito_master import *"
 __all__ = [
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ito_master",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires = [
         "torch>=2.0",
@@ -20,6 +20,7 @@ setup(
         "julius",
         "pyloudnorm",
         "matplotlib",
+        "pymixconsole",
         "soxbindings",
         "aubio",
         "laion-clap",
